0d695fd1241c206f711f918f771d5d5680749ef2
James <42484571+smitteh1@users.noreply.github.com>
2019-03-27 16:15:43 -0400

    Beautify server.js

--- SCNodeApp/server.js
+++ SCNodeApp/server.js
@@ -1,7 +1,7 @@
 // Time (ms) between frame captures
 var timeLapse = 2000;
 // How much motion is needed for it to be triggered
-var motionTh = 0.03; 
+var motionTh = 0.03;
 
 const express = require('express');
 const path = require('path');
@@ -17,82 +17,114 @@ const Jimp = require('jimp');
 
 // view engine setup
 app.set('trust proxy', true)
-.set('views', path.join(__dirname, 'views'))
-.set('view engine', 'ejs')
-.set('view options', {pretty: true})
-.locals.pretty = app.get('env') === 'development';
+	.set('views', path.join(__dirname, 'views'))
+	.set('view engine', 'ejs')
+	.set('view options',
+	{
+		pretty: true
+	})
+	.locals.pretty = app.get('env') === 'development';
 
 app.use(logger('dev'))
-.use(express.json())
-.use(express.urlencoded({ extended: false }))
-.use(cookieParser())
-.use(express.static(path.join(__dirname, 'public')))
-.use((req, res, next) => {
-  res.locals.user = req.user;
-  next();
-})
+	.use(express.json())
+	.use(express.urlencoded(
+	{
+		extended: false
+	}))
+	.use(cookieParser())
+	.use(express.static(path.join(__dirname, 'public')))
+	.use((req, res, next) =>
+	{
+		res.locals.user = req.user;
+		next();
+	})
 
 var frame = 0;
-var img1,img2;
+var img1, img2;
 
-function resolver() {
-    return new Promise(resolve => {
-        frame++;
-      setTimeout(() => {
-        
-        screenshot.listDisplays().then((displays) => {
-        // Takes screenshot of second screen (using vlc)
-            screenshot({ screen: displays[displays.length - 2].id })
-              .then((img) => {
-                fs.writeFile('screenshot'+ frame +'.jpg', img, function (err) {
-                    if (err) {
-                      throw err
-                }
-            })
-              });
-          })
-                console.log('Frame #' + frame + " Captured!");
-                if (frame > 1) {
-                    getChangedPixels(frame,frame-1);
-                }
-            
-        resolve('Resolved!');
-      }, timeLapse);
-    });
-  }
-  
-  async function startMotionDetection() {
-    var result = await resolver();
-    startMotionDetection();
-  }
+function resolver()
+{
+	return new Promise(resolve =>
+	{
+		frame++;
+		setTimeout(() =>
+		{
 
-function getChangedPixels(iNum1,INum2) {
-    return new Promise(resolve => {
-      setTimeout(() => {
-    Jimp.read('screenshot'+iNum1+".jpg")
-    .then(image1 => {
-        Jimp.read('screenshot'+INum2+".jpg")
-        .then(image2 => {
-         var difff = Jimp.diff(image1,image2,0.2);
-          // Used for debuggin
-          //console.log(difff.percent);
-         if (difff.percent > motionTh) {
-            console.log("Detected Motion on Camera");
-         }else{
-             console.log("No Motion Detected")
-             fs.unlink("screenshot"+iNum1+".jpg", (err) => {
-                if (err) throw err;
-              });
-              fs.unlink("screenshot"+INum2+".jpg", (err) => {
-                if (err) throw err;
-              });
-         }
-        })
-        .catch(err1 => { }); })
-    .catch(err => { });
-    resolve("Calct!")
-        });
-    });
+			screenshot.listDisplays().then((displays) =>
+			{
+				// Takes screenshot of second screen (using vlc)
+				screenshot(
+					{
+						screen: displays[displays.length - 2].id
+					})
+					.then((img) =>
+					{
+						fs.writeFile('screenshot' + frame + '.jpg', img, function (err)
+						{
+							if (err)
+							{
+								throw err
+							}
+						})
+					});
+			})
+			console.log('Frame #' + frame + " Captured!");
+			if (frame > 1)
+			{
+				getChangedPixels(frame, frame - 1);
+			}
+
+			resolve('Resolved!');
+		}, timeLapse);
+	});
+}
+
+async function startMotionDetection()
+{
+	var result = await resolver();
+	startMotionDetection();
+}
+
+function getChangedPixels(iNum1, INum2)
+{
+	return new Promise(resolve =>
+	{
+		setTimeout(() =>
+		{
+			Jimp.read('screenshot' + iNum1 + ".jpg")
+				.then(image1 =>
+				{
+					Jimp.read('screenshot' + INum2 + ".jpg")
+						.then(image2 =>
+						{
+							var difff = Jimp.diff(image1, image2, 0.2);
+							// Used for debuggin
+							//console.log(difff.percent);
+							if (difff.percent > motionTh)
+							{
+								console.log("Detected Motion on Camera");
+							}
+							else
+							{
+								console.log("No Motion Detected")
+								fs.unlink("screenshot" + iNum1 + ".jpg", (err) =>
+								{
+									if (err) throw err;
+								});
+								fs.unlink("screenshot" + INum2 + ".jpg", (err) =>
+								{
+									if (err) throw err;
+								});
+							}
+						})
+						.catch(err1 =>
+						{});
+				})
+				.catch(err =>
+				{});
+			resolve("Calct!")
+		});
+	});
 }
 
 
